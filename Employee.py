c598f5c8b7a0f9d9f6e1380a1c8b2dd897b1d16a
Siwakornzz <Siwakornzz@hotmail.com>
2021-09-12 15:19:22 +0700

    add Employee Class

---
+++ Employee.py
@@ -0,0 +1,15 @@
+class Employee:
+    
+    def __init__(self,id,name,salary):
+        self.id = id
+        self.name = name
+        self.salary = salary
+    
+    def getid(self):
+        return self.id
+
+    def getname(self):
+        return self.name
+    
+    def getsalary(self):
+        return self.salary
