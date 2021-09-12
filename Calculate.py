eb51003f9d53f63a54b8c13bd8a3bc3cfd46e21b
Basskerz <chanasak2543@gmail.com>
2021-09-12 15:44:53 +0700

    Update Calculate for project-Aminor

--- Calculate.py
+++ Calculate.py
@@ -9,13 +9,30 @@ class Calculate:
     def getmonthsell(self):
         return self.monthsell
 
+    def setsalary(self,salary):
+        self.salary = salary
+
+    def getsalary(self):
+        return self.salary
+
     def cal(self):
-        if self.monthsell >= 50001:
-            total = self.monthsell * 0.10
-        elif self.monthsell <= 50000:
-            total = self.monthsell * 0.05
+        if self.salary <= 10000:
+            if self.monthsell >= 50001:
+                total = self.monthsell * 0.10
+            elif self.monthsell <= 50000:
+                total = self.monthsell * 0.05
+            else:
+                total = 0
+            total = self.salary + total
+            
+            return total
         else:
-            total = 0
-        total = self.salary + total
-        
-        return total
+            if self.monthsell > 100000:
+                total = self.monthsell * 0.10
+            elif self.monthsell <= 100000:
+                total = self.monthsell * 0.05
+            else:
+                total = 0
+            total = self.salary + total
+            
+            return total
