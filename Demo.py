408961255c6708502f87536c18fc6a9409ada42b
klotza5678 <klotza5678@gmail.com>
2021-09-12 16:42:16 +0700

    Update Demo add library to wait user decistion to exit

--- Demo.py
+++ Demo.py
@@ -1,4 +1,5 @@
 from Show import Show
+import os
 
 name = input("Enter Ur name : ")
 id = int(input("Enter Ur ID : "))
@@ -7,4 +8,7 @@ s = Show(id,name,salary)
 monthsell = int(input("Enter Ur Monthsell : "))
 s.setmonthsell(monthsell)
 
+print("------------------------------")
 s.tostring()
+print("------------------------------")
+os.system('pause')
